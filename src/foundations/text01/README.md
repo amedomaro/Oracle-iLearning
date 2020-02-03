b8a9ecd0a198942917d75a14f4b29d9eb6b10059
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-03 23:27:38 +0300

    add Text01

---
+++ src/foundations/text01/README.md
@@ -0,0 +1,2 @@
+# Run the program with the debugger
+Continue pressing Step Over Expression until the complicated code appears. Step Over Expression is similar to Step Over, except that it provides for more fine-grained stepping through code. But this level of detail is sometimes unwanted.
---
+++ src/foundations/text01/Text01.java
@@ -0,0 +1,14 @@
+package foundations.text01;
+
+public class Text01 {
+    public static void main(String[] args) {
+        System.out.println("   /\\         /\\   ");
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }
+}
