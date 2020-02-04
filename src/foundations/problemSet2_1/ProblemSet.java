932d931a9b07063c4b632853011766a51d4adf99
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-04 10:26:07 +0300

    add ProblemSet2_1

---
+++ src/foundations/problemSet2_1/ProblemSet.java
@@ -0,0 +1,52 @@
+package foundations.problemSet2_1;
+
+public class ProblemSet {
+    public static void main(String[] args) {
+        System.out.println("__________________1111111");
+        System.out.println("______________11¶¶¶¶¶¶¶¶¶¶¶¶11");
+        System.out.println("___________1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1");
+        System.out.println("_________¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+        System.out.println("_______¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶11");
+        System.out.println("______¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1");
+        System.out.println("____1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶11");
+        System.out.println("___1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_11");
+        System.out.println("__1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶1");
+        System.out.println("__¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶11111¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶_11");
+        System.out.println("__¶¶¶¶¶¶¶¶¶¶¶¶111¶1____1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶11");
+        System.out.println("_¶¶¶¶¶¶¶¶¶¶¶1___1_1¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶11");
+        System.out.println("1¶¶¶¶¶¶¶¶¶1____11¶¶¶¶¶_¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+        System.out.println("¶¶¶¶¶¶¶¶¶1____1¶¶¶1____1¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶");
+        System.out.println("¶¶¶¶¶¶¶1______111¶¶1____¶¶¶¶¶¶¶¶¶¶1_1¶¶¶¶¶¶¶¶");
+        System.out.println("¶¶¶¶¶1_________11¶1_____¶¶¶¶¶¶111¶¶_1¶¶11¶¶¶¶");
+        System.out.println("1¶¶1_________1¶¶¶_________1111___¶¶1_1¶11¶1");
+        System.out.println("_____________1¶¶_________11__11__1¶¶_1¶11¶");
+        System.out.println("_________________________1¶1__¶¶¶1¶¶__111¶");
+        System.out.println("__________________________1¶1__¶¶¶¶¶1__11¶");
+        System.out.println("___________________________1¶___1¶¶¶1__¶¶¶");
+        System.out.println("__________________1_________¶¶1__1¶1__¶¶¶");
+        System.out.println("________________1¶¶¶¶___11_1¶¶¶__1¶___¶¶");
+        System.out.println("____11__________¶¶_1¶¶¶¶1__¶¶¶¶__¶¶1_1¶1");
+        System.out.println("____1¶1________11¶¶1__¶¶¶¶¶¶¶¶¶__1¶1_¶¶");
+        System.out.println("______1¶¶¶_____1__1¶¶1___1¶¶¶¶1¶__¶¶_¶¶");
+        System.out.println("________¶¶¶____111__111¶¶_________1¶¶¶¶");
+        System.out.println("________11¶¶___1111_111¶¶¶1_______1¶¶¶");
+        System.out.println("__________1¶¶1_111¶¶¶¶¶¶¶¶¶1_1¶¶¶¶¶¶¶¶");
+        System.out.println("_______1____¶¶¶11¶¶¶¶¶¶11¶¶1_1¶¶¶¶¶¶¶1");
+        System.out.println("¶1_____1_____¶¶¶¶¶¶¶¶¶1__¶¶__1_1¶¶¶¶¶");
+        System.out.println("¶¶__________¶¶¶¶¶¶¶11____¶¶1__1¶¶¶¶¶¶");
+        System.out.println("_11_______1¶¶¶¶¶¶¶¶11____¶¶¶_1¶¶¶¶¶¶¶");
+        System.out.println("__1¶¶____¶¶¶¶¶¶¶¶¶¶¶¶1___1¶¶¶¶¶¶¶¶¶¶¶");
+        System.out.println("___¶¶¶_1¶¶¶¶¶¶¶¶¶¶¶¶¶¶____¶¶¶¶¶¶¶¶¶¶¶1");
+        System.out.println("____1¶1¶¶111¶¶¶¶¶¶¶¶¶¶¶____1¶¶¶¶¶¶¶¶¶1");
+        System.out.println("______11_1111¶¶¶¶¶¶¶¶¶¶1____¶¶¶¶¶¶¶¶¶¶");
+        System.out.println("__________111¶11¶¶¶¶¶¶¶¶1___¶¶¶¶¶¶¶¶¶¶1");
+        System.out.println("__________1_11¶1¶¶¶¶¶¶¶¶¶¶__1¶¶¶¶¶¶¶¶¶¶");
+        System.out.println("____________11¶¶1¶¶¶¶¶¶¶¶¶1__¶¶¶¶¶¶¶¶¶¶¶");
+        System.out.println("¶11¶¶¶¶¶¶¶¶¶1_¶¶¶¶¶¶¶¶¶¶¶¶");
+
+        System.out.println("\n Hand-Face");
+
+        //Use 8 print statements to print a smiley face.
+        //Your art will rely on only a single character, besides space, such as X or #.
+    }
+}
---
+++ src/foundations/problemSet2_1/README.md
@@ -0,0 +1,20 @@
+# Problem 1: Simple ASCII Art
+
+~~~~
+          ######
+         #      #
+        # ##  ## #
+        #        #
+        # #    # #
+        #  ####  #
+         #      #
+          ######
+~~~~
+
+###### Overview
+
+Using text to create a picture is known as ASCII art.  In section 2, we made an ASCII art cat.  In this practice, you’ll use print statements to recreate the image above
+
+###### Task
+
+Use 8 print statements to recreate the smiley face above.  Your art will rely on only a single character, besides space, such as X or #.
