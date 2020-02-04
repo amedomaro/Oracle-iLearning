5020ca7c1cb54062c25604f7fe7c5cc70b43f54a
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-04 12:36:03 +0300

    add Variables03

---
+++ src/foundations/variables03/Variables03.java
@@ -0,0 +1,35 @@
+package foundations.variables03;
+
+public class Variables03 {
+
+    public static void main(String[] args) {
+
+        boolean bool = true;
+
+        int intVar1 = 1;
+        int intVar2 = 2;
+        int intVar3 = 3;
+
+        double doubleVar1, doubleVar2, doubleVar3, doubleVar4;
+        doubleVar1 = 1.1;
+        doubleVar2 = 2.1;
+        doubleVar3 = 3.1;
+        doubleVar4 = 4.1;
+
+
+        String stringVar1 = "1";
+        String stringVar2 = "2";
+
+        //Don't edit these print statements
+        System.out.println("bool = "        +bool);
+        System.out.println("intVar1 = "     +intVar1);
+        System.out.println("intVar2 = "     +intVar2);
+        System.out.println("intVar3 = "     +intVar3);
+        System.out.println("doubleVar1 = "  +doubleVar1);
+        System.out.println("doubleVar2 = "  +doubleVar2);
+        System.out.println("doubleVar3 = "  +doubleVar3);
+        System.out.println("doubleVar4 = "  +doubleVar4);
+        System.out.println("stringVar1 = "  +(stringVar1 +1));
+        System.out.println("stringVar2 = "  +(stringVar2 +2));
+    }
+}
