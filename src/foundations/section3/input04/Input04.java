507a7b497d7866eb347d652b7c37ff3bf9c1341c
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-07 00:08:32 +0300

    add Input04

--- src/foundations/section3/input04/Input04.java
+++ src/foundations/section3/input04/Input04.java
@@ -7,15 +7,15 @@ public class Input04 {
         Scanner sc = new Scanner(Input04.class.getResourceAsStream("input04text.txt"));
 
         //Edit these lines to advance the scanner
-        sc.nextLine();
         System.out.println(sc.nextLine());
-
+        System.out.println(sc.nextLine());
+        System.out.println(sc.nextLine());
         //Does this line contain "BlueBumper"?
         System.out.println(sc.findInLine("BlueBumper"));
         //Store the next two numbers as xPosition and yPosition
         //Print these positions
 
-        //System.out.printf("X: %d , Y: %d", sc.nextInt(), sc.nextInt());
+        System.out.printf("X: %d , Y: %d", sc.nextInt(), sc.nextInt());
         //System.out.println("X: "        + ", Y: "        );
         sc.close();
 
