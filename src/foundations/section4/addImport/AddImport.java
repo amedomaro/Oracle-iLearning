0b9e94def813bf3fbe403328f4b04419b87186ae
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-10 19:42:12 +0300

    add AddImport

---
+++ src/foundations/section4/addImport/AddImport.java
@@ -0,0 +1,22 @@
+package foundations.section4.addImport;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.swing.JLabel;
+import java.util.*;
+
+//import java.util.Calendar;
+//import java.util.Date;
+
+public class AddImport {
+
+    public static void main(String[] args) {
+        JLabel label = new JLabel("hello");
+
+    }
+
+}
