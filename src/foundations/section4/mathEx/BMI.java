be78ddffcfe810621e53426feaac71cfe9481ee1
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-11 20:17:22 +0300

    Not working

--- src/foundations/section4/mathEx/BMI.java
+++ src/foundations/section4/mathEx/BMI.java
@@ -6,34 +6,48 @@ import java.io.InputStreamReader;
 
 public class BMI {
 
-    double weight = 0.0, height = 0;
+    private double weight = 0.0, height = 0;
 
-    public void getWeight() {
+    public void getBmi(){
+        getWeight();
+        System.out.println(1);
+        getHeight();
+        System.out.println(2);
 
-        System.out.print("Enter the weight in pounds: ");
-
-        getValue();
-
-        System.out.println(weight);
     }
 
-    private void getValue() {
+    public void getWeight() {
+        System.out.print("Enter the weight in pounds: ");
+        weight = getValue();
+    }
+
+    public void getHeight() {
+        System.out.print("Enter the height in pounds: ");
+        height = getValue();
+    }
+
+    private double getValue() {
+
+        double result = 0;
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
             String input = br.readLine();
 
-            if (input.matches("\\D++")) {
-                System.out.printf("%s - is not a number!!!\n", input);
+            if (input.matches("(([-+])?[0-9]+(\\.[0-9]+)?)+")) {    // Check for double
+                result = Double.parseDouble(input);
 
+                if (result <= 0){
+                    System.out.println("The number must be greater than 0");
+                }
 
             } else {
-                weight = Double.parseDouble(input);
+                System.out.printf("%s - is not a number!!!\n", input);
             }
 
         } catch (IOException e) {
-            //e.printStackTrace();
+            e.printStackTrace();
         }
-
+        return result;
     }
 }
--- src/foundations/section4/mathEx/ComputeBMI.java
+++ src/foundations/section4/mathEx/ComputeBMI.java
@@ -1,13 +1,10 @@
 package foundations.section4.mathEx;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class ComputeBMI {
     public static void main(String[] args) {
         BMI bmi = new BMI();
-        bmi.getWeight();
+        bmi.getBmi();
+
 
     }
 }
