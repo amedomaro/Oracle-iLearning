89c294aa88ead29b53d57996e9f242795d5b0fe9
xzhopax <xzhopax@gmail.com>
2020-02-11 20:39:26 +0300

    ???

--- src/foundations/section4/mathEx/BMI.java
+++ src/foundations/section4/mathEx/BMI.java
@@ -6,34 +6,45 @@ import java.io.InputStreamReader;
 
 public class BMI {
 
-    double weight = 0.0, height = 0;
+    double weight = 0.0, height = 0, test = 0.0;
 
     public void getWeight() {
 
         System.out.print("Enter the weight in pounds: ");
 
-        getValue();
+       weight = getValue();
+    }
+    public void getHeight() {
 
-        System.out.println(weight);
+        System.out.print("Enter the height in pounds: ");
+
+        height = getValue();
     }
 
-    private void getValue() {
+    private double getValue() {
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        double input;
+        String str = null;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                str = br.readLine();
+                input = Double.parseDouble(str);
 
-            String input = br.readLine();
+                    if (input <= 0.0) {
+                        System.out.printf("%s - is number <= 0!!!\n", input);
+                        System.out.print("Please enter number :");
+                        getValue();
 
-            if (input.matches("\\D++")) {
-                System.out.printf("%s - is not a number!!!\n", input);
+                    } else
+                        test = input;
+                        return input;
 
+            } catch (NumberFormatException | IOException e) {
 
-            } else {
-                weight = Double.parseDouble(input);
+                System.out.printf("%s - is not a number!!!\n", str);
+                System.out.print("Please enter number :");
+                getValue();
             }
-
-        } catch (IOException e) {
-            //e.printStackTrace();
-        }
-
+        return test ;
     }
 }
--- src/foundations/section4/mathEx/ComputeBMI.java
+++ src/foundations/section4/mathEx/ComputeBMI.java
@@ -8,6 +8,9 @@ public class ComputeBMI {
     public static void main(String[] args) {
         BMI bmi = new BMI();
         bmi.getWeight();
+        bmi.getHeight();
+        System.out.println(bmi.weight);
+        System.out.println(bmi.height);
 
     }
 }
