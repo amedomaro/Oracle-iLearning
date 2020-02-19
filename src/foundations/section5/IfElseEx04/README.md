6e124fd1f7315ff9b491828a498716a17be422df
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-19 12:45:39 +0300

    add ifElseEx04

---
+++ src/foundations/section5/IfElseEx04/README.md
@@ -0,0 +1,13 @@
+# Exercise 4 
+• Import and open the `IfElseEx` project.
+
+• Examine `ShoppingCart.java`.
+
+• Use an `if/else` statement to implement the following: 
+
+– Declare and initialize a `boolean` variable, `outOfStock`.
+
+– If quantity > 1, change the message variable to indicate plural. 
+
+– If an item is out of stock, inform the user that the item is unavailable.  
+Else print the message and the total cost.
--- src/foundations/section5/IfElseEx04/ShoppingCart.java
+++ src/foundations/section5/IfElseEx04/ShoppingCart.java
@@ -3,7 +3,7 @@ package foundations.section5.IfElseEx04;
 public class ShoppingCart {
 
     public static void main(String[] args) {
-        String custName = "Mary Smith";
+        String castName = "Mary Smith";
         String itemDesc = "Shirt";
 
         // numeric fields
@@ -11,20 +11,29 @@ public class ShoppingCart {
         int quantity = 2;
         double tax = 1.04;
         double total;
-        String message = custName + " wants to purchase " + quantity + " " + itemDesc;
+        String message = castName + " wants to purchase " + quantity + " " + itemDesc;
 
         // Calculating total cost
         total = (price * quantity) * tax;
 
-
         // Declare outOfStock variable and initialize it
+        boolean outOfStock = false;
 
+        if (quantity > 1) {
+            outOfStock = true;
+        }
 
         // Test quantity and modify message if quantity > 1.
-
+        if (outOfStock) {
+            System.out.printf("%ss\n", message);
+        }
 
         // Test outOfStock and notify user in either case.
-
+        if (quantity < 1) {
+            System.out.println("An item is out of stock");
+        } else {
+            System.out.println(message);
+        }
 
     }
 }
