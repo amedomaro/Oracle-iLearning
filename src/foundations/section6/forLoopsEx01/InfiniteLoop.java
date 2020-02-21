b3d7be244a1a3e11a30ebdc7388852161be16134
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-21 22:24:46 +0300

    add forLoopsEx01

---
+++ src/foundations/section6/forLoopsEx01/InfiniteLoop.java
@@ -0,0 +1,13 @@
+package foundations.section6.forLoopsEx01;
+
+public class InfiniteLoop {
+    public static void main(String[] args) {
+
+        // The for loop will run infinite times.
+        for(int i = 0; i < 5; i++){
+            System.out.println("Hello");
+        }
+
+        // To terminate this program press ctrl + c in the console.
+    }
+}
---
+++ src/foundations/section6/forLoopsEx01/README.md
@@ -0,0 +1,6 @@
+# Exercise 2
+• Import and open the `ForLoopsEx` project.
+
+• Execute `InfiniteLoop.java` and observe the output.
+
+• Modify the `for` loop in `InfiniteLoop.java` to print `“Hello”` five times
