a6354255c6d2c939b69f4f5ed77eb935c4647eb0
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-02-22 13:49:24 +0300

    add WhileLoopEx02

---
+++ src/foundations/section6/WhileLoopEx02/README.md
@@ -0,0 +1,6 @@
+# Exercise 2
+• Import and open the `WhileLoopEx` project.
+
+• Examine the `SumOfNums.java`, which sums up a sequence of 10 integers that are input by the user.
+
+• Can you implement the same by using a `do-while` loop?
---
+++ src/foundations/section6/WhileLoopEx02/SumOfNumbs.java
@@ -0,0 +1,24 @@
+package foundations.section6.WhileLoopEx02;
+
+import java.util.Scanner;
+
+public class SumOfNumbs {
+
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        int sum = 0;
+
+        System.out.print("Enter a number (-1 to quit): ");
+        int number = console.nextInt();
+
+        do {
+            sum = sum + number;     // moved to top of loop
+            System.out.print("Enter a number (-1 to quit): ");
+            number = console.nextInt();
+        } while (number != -1);
+
+        System.out.println("The sum is " + sum);
+
+    }
+}
