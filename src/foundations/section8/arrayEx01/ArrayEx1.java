6f3040d262ed46621ff37d789acf722402eb4e4e
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-03-04 16:35:42 +0300

    add arrayEx01

---
+++ src/foundations/section8/arrayEx01/ArrayEx1.java
@@ -0,0 +1,22 @@
+package foundations.section8.arrayEx01;
+
+public class ArrayEx1 {
+
+    public static void main(String[] args) {
+
+        int[] primeNum = new int[20];
+        int[] even = new int[5];
+        long[] primes = new long[20];
+        primes[0] = 2;
+        primes[1] = 3;
+        System.out.println(primes[0]);
+        primes[0] = 5;
+        System.out.println(primes[0]);
+
+        // Solution
+
+        int[] score = new int[9];
+        byte[] values = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+    }
+}
---
+++ src/foundations/section8/arrayEx01/README.md
@@ -0,0 +1,9 @@
+# Exercise 2
+• Import and open the `ArrayEx` project.
+
+• Examine `ArrayEx1.java`.
+
+• Modify the program to implement ...
+- Declare a one-dimensional array named `score` of type `int` that can hold 9 values.
+- Declare and initialize a one-dimensional byte array named `values` of size 10 so that all entries contain 1.
+- Uncomment the two lines that are commented out and then resolve the syntax errors.
