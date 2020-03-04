666159e20c9e492c337271102f9ddbdd78f872fb
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-03-04 17:31:25 +0300

    add arrayEx03

---
+++ src/foundations/section8/arrayEx03/ArrayEx2.java
@@ -0,0 +1,15 @@
+package foundations.section8.arrayEx03;
+
+public class ArrayEx2 {
+
+    public static void main(String[] args) {
+
+        String[] browsers = new String[3];
+        browsers[0] = "explorer";
+        browsers[1] = "chrome";
+        browsers[2] = "opera";
+        // browsers[3] = "firefox";
+        System.out.print("size of array " + browsers.length);
+
+    }
+}
---
+++ src/foundations/section8/arrayEx03/README.md
@@ -0,0 +1,9 @@
+# Exercise 4
+• Import and open the `ArrayEx` project.
+
+• Examine `ArrayEx2.java`.
+
+• Perform the following:
+- Run the program and observe the error.
+- Modify the program to resolve the error.
+- Using a `for-each` loop, display all browsers that are stored in the array.
