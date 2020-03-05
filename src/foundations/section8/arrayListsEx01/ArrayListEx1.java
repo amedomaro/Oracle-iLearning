a2c8a110c5f08b11fcbfe17a34165e296a452ea3
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-03-05 12:18:23 +0300

    add arrayListsEx01

--- src/foundations/section8/arrayListsEx01/ArrayListEx1.java
+++ src/foundations/section8/arrayListsEx01/ArrayListEx1.java
@@ -8,12 +8,22 @@ public class ArrayListEx1 {
     public static void main(String[] args) {
         List<String> students = new ArrayList<>();
 
+
         students.add("Amy");
         students.add("Bob");
         students.add("Cindy");
         students.add("David");
 
         System.out.println(students);
-        System.out.printf("The list of \"students\" is length: %d\n", students.size());
+        System.out.printf("The list of \"students\" is length: %d\n\n", students.size());
+
+        // Part 2
+
+        students.add(0, "Nick");
+        students.add(1, "Mike");
+        students.remove(3);
+
+        System.out.println(students);
+        System.out.printf("The list of \"students\" is length: %d\n\n", students.size());
     }
 }
