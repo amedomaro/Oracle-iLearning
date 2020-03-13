d56098456a965d47fba747548a344e2f0e3a9baf
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-03-13 17:35:12 +0300

    add javaFX08

---
+++ src/foundations/section9/javaFX08/Goal.java
@@ -0,0 +1,37 @@
+package foundations.section9.javaFX08;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.AudioClip;
+
+public class Goal {
+    private Image duke = new Image(getClass().getResource("Images/Duke.png").toString());
+    protected ImageView dukeView = new ImageView(duke);
+
+    private Image glove = new Image(getClass().getResource("Images/Glove.png").toString());
+    protected ImageView gloveView = new ImageView(glove);
+
+    private AudioClip tone = new AudioClip(getClass().getResource("Audio/Note5.wav").toString());
+
+    protected Goal(){
+        dukeView.setPreserveRatio(true);            // Scaling a Node the Right Way
+        dukeView.setFitWidth(125);
+        dukeView.setX(250);
+        dukeView.setY(150);
+
+        gloveView.setPreserveRatio(true);
+        gloveView.setFitWidth(85);
+        gloveView.setX(300);
+        gloveView.setY(150);
+
+        GoalTest.root.getChildren().addAll(dukeView, gloveView);
+        //tone.
+
+        //interactions();
+    }
+
+    private void interactions(){
+        //Exercise 4
+
+    }
+}
---
+++ src/foundations/section9/javaFX08/README.md
@@ -0,0 +1,7 @@
+# Exercise 3
+• Continue editing the `GoalTest` project.
+
+• Declare an Audio object as a field.
+
+• Instantiate the Audio object.
+- Use the `.wav` file in the project directory.
