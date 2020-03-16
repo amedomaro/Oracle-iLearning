98586ad5b3633d6a2b7f9d9c6f681e999132f3c9
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-03-16 12:43:37 +0300

    add javaFX09

---
+++ src/foundations/section9/javaFX09/Goal.java
@@ -0,0 +1,39 @@
+package foundations.section9.javaFX09;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.awt.event.MouseEvent;
+
+public class Goal {
+    private Image duke = new Image(getClass().getResource("Images/Duke.png").toString());
+    protected ImageView dukeView = new ImageView(duke);
+
+    private Image glove = new Image(getClass().getResource("Images/Glove.png").toString());
+    protected ImageView gloveView = new ImageView(glove);
+
+    MediaPlayer player = new MediaPlayer(new Media(getClass().getResource("Audio/Note5.wav").toString()));
+
+    protected Goal(){
+        dukeView.setPreserveRatio(true);            // Scaling a Node the Right Way
+        dukeView.setFitWidth(125);
+        dukeView.setX(250);
+        dukeView.setY(150);
+
+        gloveView.setPreserveRatio(true);
+        gloveView.setFitWidth(85);
+        gloveView.setX(300);
+        gloveView.setY(150);
+
+        GoalTest.root.getChildren().addAll(dukeView, gloveView);
+
+        interactions();
+    }
+
+    private void interactions(){
+        dukeView.setOnMousePressed((MouseEvent) -> player.play());
+
+    }
+}
---
+++ src/foundations/section9/javaFX09/README.md
@@ -0,0 +1,10 @@
+# Exercise 4
+• Continue editing the `GoalTest` project.
+
+• Complete the `interactions()` method so that ...
+- Duke listens for a mouse press and mouse drag.
+- Play a sound when the mouse is pressed.
+- Print the x and y positions of the mouse dragged event. This will be helpful for the problem set.
+
+• What if `interactions()` is never called?
+-  Comment out this method call in the constructor.
