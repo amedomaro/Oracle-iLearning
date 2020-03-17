7f46f29223a7cf3a8faf4ff209a4927ca378ac70
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-03-17 14:52:01 +0300

    refactoring Button

--- src/foundations/section9/practices/MyButton.java
+++ src/foundations/section9/practices/MyButton.java
@@ -3,13 +3,14 @@ package foundations.section9.practices;
 import javafx.scene.control.Button;
 
 public class MyButton extends Button {
-    private String color;
+    private String color, name;
 
     protected MyButton(String name, double x, double y, String color) {
-        this.setText(name);
+        setName(name);
+        setColor(color);
+        this.setText(getName());
         this.setLayoutX(x);
         this.setLayoutY(y);
-        this.setColor(color);
         this.setStyle(String.format("-fx-background-color: %s", getColor()));
 
         Main.root.getChildren().add(this);
@@ -23,5 +24,13 @@ public class MyButton extends Button {
 
     private void setColor(String color) {
         this.color = color;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
     }
 }
