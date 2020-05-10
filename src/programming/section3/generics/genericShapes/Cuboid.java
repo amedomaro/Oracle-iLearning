ce2dac60d5cb039725ca915e2d6973a35ba64d11
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-05-10 19:21:32 +0300

    add project genericShapes

---
+++ src/programming/section3/generics/genericShapes/Cuboid.java
@@ -0,0 +1,53 @@
+package programming.section3.generics.genericShapes;
+
+public class Cuboid<T extends Number> {
+
+    private T length, width, height;
+    private String name;
+
+    Cuboid(String name, T length, T width, T height) {
+        this.name = name;
+        setLength(length);
+        setWidth(width);
+        setHeight(height);
+    } // end constructor
+
+    protected double getVolume() {
+        return Double.parseDouble(length.toString()) * Double.parseDouble(width.toString())
+                * Double.parseDouble(height.toString());
+    }
+
+    protected void printVolume() {
+        System.out.printf("volume of the shape = %.2f\n", getVolume());
+    }
+
+    // Below getters, setters and override
+    protected T getLength() {
+        return length;
+    }
+
+    protected void setLength(T length) {
+        this.length = length;
+    }
+
+    protected T getWidth() {
+        return width;
+    }
+
+    protected void setWidth(T width) {
+        this.width = width;
+    }
+
+    protected T getHeight() {
+        return height;
+    }
+
+    protected void setHeight(T height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "\nShape " + name + " has:\nLength - " + getLength() + "\nWidth - " + getWidth() + "\nHeight - " + getHeight();
+    }
+}
---
+++ src/programming/section3/generics/genericShapes/Main.java
@@ -0,0 +1,17 @@
+package programming.section3.generics.genericShapes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cuboid<Double> cuboidDouble = new Cuboid<>("cuboidDouble",1.3, 2.2, 2.0);
+        Cuboid<Integer> cuboidInteger = new Cuboid<>("cuboidInteger",1,2,3);
+
+
+
+        System.out.println(cuboidDouble.toString());
+        cuboidDouble.printVolume();
+        System.out.println(cuboidInteger.toString());
+        cuboidInteger.printVolume();
+
+    }
+}
