a21be4de17b9ebd7af356895e75ee65e2dfbf950
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-05-27 17:34:08 +0300

    add FileSystem class

---
+++ src/programming/section5/inputAndOutput/FileSystem.java
@@ -0,0 +1,29 @@
+package programming.section5.inputAndOutput;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileSystem {
+    public static void main(String[] args) {
+        List<String> buffer = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("C:/Users/Amedomaro/Oracle-iLearning/src/programming/section5/inputAndOutput/demoFile.txt"))){
+            String line = br.readLine();
+
+            while(line != null){
+                buffer.add(line);
+                line = br.readLine();
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        for(String element : buffer){
+            System.out.println(element);
+        }
+    }
+}
