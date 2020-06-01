21a3f372411fd24f21a6debd00c9e312c102abd5
Amedomaro <30287338+amedomaro@users.noreply.github.com>
2020-06-01 18:36:08 +0300

    add method serializeData

--- src/programming/section4/stringExample/accountGenerator/AccountGenerator.java
+++ src/programming/section4/stringExample/accountGenerator/AccountGenerator.java
@@ -1,14 +1,29 @@
 package programming.section4.stringExample.accountGenerator;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class AccountGenerator {
 
-    public static void main(String[] args) throws IOException {
+    private void serializeData(Employee employee) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("employee.ser"))) {
+            oos.writeObject(employee);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+
+
+    public static void main(String[] args) throws IOException {
+        AccountGenerator accountGen = new AccountGenerator();
         Employee employee = new Employee();
 
         System.out.println(employee.toString());
+        accountGen.serializeData(employee);
+
 
         InputValue.br.close();  // Close thread System.in
 
